0f3e44def71cf0e852ede8a97c25657c0345ae01
ven-b <bigstarbag@gmail.com>
2023-11-03 00:27:30 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: :show
+
+  root to: 'prototypes#index'
+
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+
+end
